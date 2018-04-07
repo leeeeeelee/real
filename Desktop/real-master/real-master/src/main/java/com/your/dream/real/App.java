6a1130c3fb1680569d55439beef0d0780eff0664
leeeeeelee <yeyettt@cnu.ac.kr>
2018-04-07 15:00:11 +0900

    please_01

---
+++ Desktop/real-master/real-master/src/main/java/com/your/dream/real/App.java
@@ -0,0 +1,19 @@
+package com.your.dream.real;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private App() 
+	{
+		// do not create App class directly
+	}
+	
+    public static void main( String[] args ) {
+    	for(String arg : args) {
+    		logger.log("parameter: " + arg);
+    	}
+    }
+}
---
+++ Desktop/real-master/real-master/src/main/java/com/your/dream/real/NotTestedClass.java
@@ -0,0 +1,12 @@
+package com.your.dream.real;
+
+public class NotTestedClass {
+	public NotTestedClass() {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public String toString() {
+		return "this is not tested yet.";
+	}
+}
